256d4c9a19492f494954fbc803e6af138e8b11bf
Jan Vargovský <jan.vargovsky@gmail.com>
2018-04-15 21:52:14 +0200

    cartpole code improve.

--- src/MastersSemestralProject/MastersSemestralProject/CartPole-v0.py
+++ src/MastersSemestralProject/MastersSemestralProject/CartPole-v0.py
@@ -1,6 +1,7 @@
 import gym
 import tensorflow as tf
 import numpy as np
+import os
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -9,7 +10,7 @@ tf.logging.set_verbosity(tf.logging.INFO)
 X_DTYPE = tf.float32
 X_DIM = 4
 
-HIDDEN_LAYERS = [2,2]
+HIDDEN_LAYERS = [4,4,4]
 
 Y_DTYPE = tf.int64
 Y_DIM = 1
@@ -19,23 +20,23 @@ def create_mlp():
 
     hidden_layers = []
     last_layer = x
-    for layer in HIDDEN_LAYERS:
-        hidden_layer = tf.layers.dense(last_layer, layer, activation= tf.nn.relu)
+    for index, layer in enumerate(HIDDEN_LAYERS):
+        hidden_layer = tf.layers.dense(last_layer, layer, activation= tf.nn.relu, name='l-{}'.format(index))
         last_layer = hidden_layer
         hidden_layers.append(last_layer)
 
-    y = tf.layers.dense(last_layer, Y_DIM, activation= tf.nn.relu)
+    y = tf.layers.dense(last_layer, Y_DIM, activation= tf.nn.relu, name='y')
 
     return (x, hidden_layers, y)
 
 def discrete_output(x):
     return 0 if x < 0 else 1
 
-env = gym.make("CartPole-v0")
-print("Action space: {}".format(env.action_space))
-print("Observation space: {}".format(env.observation_space))
+env = gym.make('CartPole-v0')
+print('Action space: {}'.format(env.action_space))
+print('Observation space: {}'.format(env.observation_space))
 
-(x, _, y) = create_mlp() 
+(x, hidden_layers, y) = create_mlp() 
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
@@ -57,6 +58,6 @@ with sess.as_default():
             observation, reward, done, info = env.step(action)
             score += reward
             if done:
-                print("Episode finished after {} timesteps, score={}".format(t + 1, score))
+                print('Episode finished after {} timesteps, score={}'.format(t + 1, score))
                 scores.append(score)
                 break
