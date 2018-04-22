ce92a1d40d116d104bc6d8c7ee7b663edb4d0550
Jan Vargovský <jan.vargovsky@gmail.com>
2018-04-22 22:44:30 +0200

    DE test: code cleanup + rename.

--- src/MastersSemestralProject/MastersSemestralProject/DifferentialEvolutionTest.py
+++ src/MastersSemestralProject/MastersSemestralProject/DifferentialEvolutionTest.py
@@ -3,7 +3,7 @@ import numpy as np
 from DifferentialEvolution import DifferentialEvolution
 
 class DifferentialEvolutionTest(unittest.TestCase):
-    def test_de_classic_array(self):
+    def test_de(self):
         def fitness_func(params):
             return sum(map(lambda x: x ** 2, params))
 
@@ -20,22 +20,6 @@ class DifferentialEvolutionTest(unittest.TestCase):
 
         self.assertAlmostEqual(expected_fitness, actual_fitness, 8)
         [self.assertAlmostEqual(exp, act, 4) for exp, act in zip(expected_params, actual_params)]
-
-    #def test_de_list_of_arrays(self):
-    #    def fitness_func(params):
-    #        return sum(map(lambda x: x ** 2, sum(params, [])))
-
-    #    def get_population():
-    #        return [
-    #            np.random.sample(2) * 2 - 1,
-    #            np.random.sample((2,2)) * 2 - 1
-    #            ]
-
-    #    de = DifferentialEvolution(fitness_func)
-    #    NP = 50
-    #    population = [get_population() for _ in range(0, NP)]
-
-    #    actual_params = de.run(population, 10)
 
 if __name__ == '__main__':
     unittest.main()
