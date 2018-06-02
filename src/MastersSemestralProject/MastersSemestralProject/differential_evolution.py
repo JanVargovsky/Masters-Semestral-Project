5887544299f5ce0d34d052c2d3a303bc943acfec
Jan Vargovský <jan.vargovsky@gmail.com>
2018-06-02 19:30:36 +0200

    differential evolution JADE algorithm with archive + tests.

--- src/MastersSemestralProject/MastersSemestralProject/differential_evolution.py
+++ src/MastersSemestralProject/MastersSemestralProject/differential_evolution.py
@@ -1,8 +1,27 @@
 import numpy as np
 import random
 import collections
+from scipy.stats import cauchy
 
-def de(fitness_func, input_population, iterations, type = 'min'):
+def _append_min(population, new_params, old_params):
+    is_better = new_params[1] < old_params[1]
+    population.append(new_params if is_better else old_params)
+    return is_better
+def _append_max(population, new_params, old_params):
+    is_better = new_params[1] > old_params[1]
+    population.append(new_params if is_better else old_params)
+    return is_better
+def _append_better_func(type):
+    return _append_min if type == 'min' else _append_max
+
+def _get_best_min(population):
+    return min(population, key=lambda x: x[1])
+def _get_best_max(population):
+    return max(population, key=lambda x: x[1])
+def _get_best_func(type):
+    return _get_best_min if type == 'min' else _get_best_max
+
+def de(fitness_func, input_population, iterations, type='min'):
     F = 0.5 # [0,2]
     CR = 0.9 # [0,1]
     N = len(input_population)
@@ -10,18 +29,8 @@ def de(fitness_func, input_population, iterations, type = 'min'):
     dtype = input_population[0].dtype
 
     population = list(map(lambda params: (params, fitness_func(params)), input_population))
-
-    def append_min(population, new_params, old_params):
-        population.append(new_params if new_params[1] < params[1] else params)
-    def append_max(population, new_params, old_params):
-        population.append(new_params if new_params[1] > params[1] else params)
-    append_better = append_min if type == 'min' else append_max
-
-    def get_best_min(population):
-        return min(population, key=lambda x: x[1])[0]
-    def get_best_max(population):
-        return max(population, key=lambda x: x[1])[0]
-    get_best = get_best_min if type == 'min' else get_best_max
+    append_better = _append_better_func(type)
+    get_best = _get_best_func(type)
 
     def get_three_random_agents(i):
         result = set()
@@ -36,8 +45,8 @@ def de(fitness_func, input_population, iterations, type = 'min'):
         r = np.random.uniform()
         return a[j] + F * (b[j] - c[j]) if r < CR or j == R else x
 
-    for i in range(iterations):
-        print("iteration=", i)
+    for g in range(iterations):
+        print("iteration=", g)
         new_population = []
         for i, params in enumerate(population):
             indexes = get_three_random_agents(i)
@@ -51,4 +60,96 @@ def de(fitness_func, input_population, iterations, type = 'min'):
 
         population = new_population
         
-    return get_best(population)
+    return get_best(population)[0]
+
+def de_jade_with_archive(fitness_func, input_population, iterations, type='min'):
+    NP = len(input_population)
+    D = len(input_population[0])
+    dtype = input_population[0].dtype
+
+    C = 0.8 # [0,1]
+    u_CR = 0.5
+    u_F = 0.5
+    A = list() # 'set' of good params, len(A) < NP
+
+    population = list(map(lambda params: (params, fitness_func(params)), input_population))
+    append_better = _append_better_func(type)
+    get_best = _get_best_func(type)
+
+    def lehmer_mean(x, p=2):
+        return sum(map(lambda x: x ** p, x)) / sum(map(lambda x: x ** (p - 1), x))
+        #return sum(x ** p) / sum(x ** (p - 1)) in case of np.array
+    def get_random_best(population):
+        best_fitness = get_best(population)[1]
+        # get all 100 percentile from population
+        all_bests = [x for x in population if x[1] == best_fitness]
+        index = random.randrange(0, len(all_bests))
+        return all_bests[index][0]
+    def get_r1(P, i):
+        while(True):
+            index = np.random.randint(NP)
+            r1 = P[index][0]
+            if not np.array_equal(r1, i):
+                break
+        return r1
+    def get_r2(P, A, i, r1):
+        while(True):
+            index = np.random.randint(NP + len(A))
+            r2 = (P[index] if index < NP else A[index - NP])[0]
+            if not np.array_equal(r2, i) and not np.array_equal(r2, r1):
+                break
+        return r2
+    def compute_param(x, j):
+        r = np.random.random_sample()
+        return x + F * (best[j] - x) + F * (r1[j] - r2[j]) \
+            if j == j_rand or r < CR else \
+            x
+    def get_CR():
+        CR = np.random.normal(u_CR, 0.1)
+        CR = max(0, CR)
+        CR = min(1, CR)
+        return CR
+    def get_F():
+        #F = np.random.standard_cauchy(u_F)
+        while True:
+            F = cauchy.rvs(loc=u_F, scale=0.1)
+            if F >= 1:
+                F = 1
+                break
+            elif F > 0: # F is between (0;1>
+                break
+        return F
+
+    for g in range(iterations):
+        print("iteration=", g)
+        new_population = []
+        s_CR = list()
+        s_F = list()
+
+        best = get_random_best(population)
+        for params in population:
+            CR = get_CR()
+            F = get_F()
+
+            #best = get_random_best(population)
+            r1 = get_r1(population, params[0])
+            r2 = get_r2(population, A, params[0], r1)
+            j_rand = np.random.randint(D)
+
+            new_params = np.fromiter([compute_param(x,j) for j,x in enumerate(params[0])], dtype)
+            new_params = (new_params, fitness_func(new_params))
+
+            if append_better(new_population, new_params, params):
+                A.append(params)
+                s_CR.append(CR)
+                s_F.append(F)
+
+        population = new_population
+        while len(A) > NP:
+            del A[np.random.randint(0, len(A))]
+        if s_CR and s_F:
+            u_CR = (1 - C) * u_CR + C * np.mean(s_CR)
+            u_F = (1 - C) * u_F + C * lehmer_mean(s_F)
+
+    best = get_best(population)
+    return best[0]
--- src/MastersSemestralProject/MastersSemestralProject/test_differential_evolution.py
+++ src/MastersSemestralProject/MastersSemestralProject/test_differential_evolution.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from differential_evolution import de
+from differential_evolution import de, de_jade_with_archive
 
 class DifferentialEvolutionTest(unittest.TestCase):
     def test_de_find_min(self):
@@ -32,6 +32,40 @@ class DifferentialEvolutionTest(unittest.TestCase):
         expected_fitness = 0.0
 
         actual_params = de(fitness_func, population, 100, 'max')
+        actual_fitness = fitness_func(actual_params)
+
+        self.assertAlmostEqual(expected_fitness, actual_fitness, 8)
+        [self.assertAlmostEqual(exp, act, 4) for exp, act in zip(expected_params, actual_params)]
+
+    def test_de_jade_with_archive_find_min(self):
+        def fitness_func(params):
+            return sum(map(lambda x: x ** 2, params))
+
+        NP = 50
+        D = 5
+        population = np.random.sample((NP, D)) * 2 - 1
+
+        expected_params = np.zeros(D)
+        expected_fitness = 0.0
+
+        actual_params = de_jade_with_archive(fitness_func, population, 100, 'min')
+        actual_fitness = fitness_func(actual_params)
+
+        self.assertAlmostEqual(expected_fitness, actual_fitness, 8)
+        [self.assertAlmostEqual(exp, act, 4) for exp, act in zip(expected_params, actual_params)]
+
+    def test_de_jade_with_archive_find_max(self):
+        def fitness_func(params):
+            return -sum(map(lambda x: x ** 2, params))
+
+        NP = 50
+        D = 5
+        population = np.random.sample((NP, D)) * 2 - 1
+
+        expected_params = np.zeros(D)
+        expected_fitness = 0.0
+
+        actual_params = de_jade_with_archive(fitness_func, population, 100, 'max')
         actual_fitness = fitness_func(actual_params)
 
         self.assertAlmostEqual(expected_fitness, actual_fitness, 8)
