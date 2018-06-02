8736624b710e086cba863392b7391125a9bc2878
Jan Vargovský <jan.vargovsky@gmail.com>
2018-06-02 14:06:33 +0200

    classic differential evolution function may search either for min or max.

--- src/MastersSemestralProject/MastersSemestralProject/differential_evolution.py
+++ src/MastersSemestralProject/MastersSemestralProject/differential_evolution.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import collections
 
-def de(fitness_func, input_population, iterations):
+def de(fitness_func, input_population, iterations, type = 'min'):
     F = 0.5 # [0,2]
     CR = 0.9 # [0,1]
     N = len(input_population)
@@ -10,6 +10,18 @@ def de(fitness_func, input_population, iterations):
     dtype = input_population[0].dtype
 
     population = list(map(lambda params: (params, fitness_func(params)), input_population))
+
+    def append_min(population, new_params, old_params):
+        population.append(new_params if new_params[1] < params[1] else params)
+    def append_max(population, new_params, old_params):
+        population.append(new_params if new_params[1] > params[1] else params)
+    append_better = append_min if type == 'min' else append_max
+
+    def get_best_min(population):
+        return min(population, key=lambda x: x[1])[0]
+    def get_best_max(population):
+        return max(population, key=lambda x: x[1])[0]
+    get_best = get_best_min if type == 'min' else get_best_max
 
     def get_three_random_agents(i):
         result = set()
@@ -19,6 +31,7 @@ def de(fitness_func, input_population, iterations):
                 result.add(j)
         return result
 
+    # DE/rand/1/exp
     def compute_param(x, j):
         r = np.random.uniform()
         return a[j] + F * (b[j] - c[j]) if r < CR or j == R else x
@@ -34,10 +47,8 @@ def de(fitness_func, input_population, iterations):
             new_params = np.fromiter([compute_param(x,j) for j,x in enumerate(params[0])], dtype)
             new_params = (new_params, fitness_func(new_params))
 
-            #new_population.append(new_params if new_params[1] < params[1] else params)
-            new_population.append(new_params if new_params[1] > params[1] else params)
+            append_better(new_population, new_params, params)
 
         population = new_population
         
-    #return min(population, key=lambda x: x[1])[0]
-    return max(population, key=lambda x: x[1])[0]
+    return get_best(population)
--- src/MastersSemestralProject/MastersSemestralProject/test_differential_evolution.py
+++ src/MastersSemestralProject/MastersSemestralProject/test_differential_evolution.py
@@ -1,13 +1,12 @@
 import unittest
 import numpy as np
-from differential_evolution import DifferentialEvolution
+from differential_evolution import de
 
 class DifferentialEvolutionTest(unittest.TestCase):
-    def test_de_find_max(self):
+    def test_de_find_min(self):
         def fitness_func(params):
-            return 1.0 / sum(map(lambda x: x ** 2, params))
+            return sum(map(lambda x: x ** 2, params))
 
-        de = DifferentialEvolution(fitness_func)
         NP = 50
         D = 5
         population = np.random.sample((NP, D)) * 2 - 1
@@ -15,8 +14,25 @@ class DifferentialEvolutionTest(unittest.TestCase):
         expected_params = np.zeros(D)
         expected_fitness = 0.0
 
-        actual_params = de.run(population, 100)
-        actual_fitness = 1.0 / fitness_func(actual_params)
+        actual_params = de(fitness_func, population, 100, 'min')
+        actual_fitness = fitness_func(actual_params)
+
+        self.assertAlmostEqual(expected_fitness, actual_fitness, 8)
+        [self.assertAlmostEqual(exp, act, 4) for exp, act in zip(expected_params, actual_params)]
+
+    def test_de_find_max(self):
+        def fitness_func(params):
+            return -sum(map(lambda x: x ** 2, params))
+
+        NP = 50
+        D = 5
+        population = np.random.sample((NP, D)) * 2 - 1
+
+        expected_params = np.zeros(D)
+        expected_fitness = 0.0
+
+        actual_params = de(fitness_func, population, 100, 'max')
+        actual_fitness = fitness_func(actual_params)
 
         self.assertAlmostEqual(expected_fitness, actual_fitness, 8)
         [self.assertAlmostEqual(exp, act, 4) for exp, act in zip(expected_params, actual_params)]
